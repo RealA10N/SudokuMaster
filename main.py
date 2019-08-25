d67537ece3243d689b85a8839014452050d49bd9
Alon <downtown2u@gmail.com>
2019-08-25 15:56:48 +0300

    Created SudokuSolver class

--- main.py
+++ main.py
@@ -110,6 +110,50 @@ class SudokuTable():
     return process_table
 
 
+class SudokuSolver():
+
+  def __init__(self, table):
+    self._table = table
+  
+  #-- C H E C K S --#
+
+  def check_valid_row(self, row_index):
+    return self._check_valid_list(self._table.get_row(row_index))
+
+  def check_valid_column(self, col_index):
+    return self._check_valid_list(self._table.get_column(col_index))
+
+  def check_valid_square(self, row_index, col_index):
+    return self._check_valid_list(self._table.get_square(row_index, col_index))
+
+  # checks for duplicates in a list. used to check valid row, columns and squares
+  def _check_valid_list(self, in_list):
+    used_before = list()
+    for item in in_list:
+      if item is not None:
+        if item not in used_before:
+          used_before.append(item)
+        else:
+          return False
+    return True
+  
+  # checks every row, column and square in the table
+  def check_valid_table(self):
+    for cur_index in range(self._table.get_full_size()):
+
+      if self.check_valid_row(cur_index) == False:
+        return False
+
+      if self.check_valid_column(cur_index) == False:
+        return False
+
+      square_row = (cur_index // self._table.get_size()) * self._table.get_size()
+      square_column = (cur_index % self._table.get_size()) * self._table.get_size()
+      if self.check_valid_square(square_row, square_column) == False:
+        return False
+    return True
+
+
 # # # # # # # # # # # #
 # E X C E P T I O N S #
 # # # # # # # # # # # #
@@ -122,4 +166,3 @@ class TableInvalidSize(Exception):
 
 class InvalidSudokuValue(Exception):
   pass
-  
